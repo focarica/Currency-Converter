0dfd7bf7f87870176337452ed07e92e839caae20
focarica <a.vinicius0042@gmail.com>
2025-01-29 18:35:18 -0300

    initial currency converter workflow

---
+++ api/scraper.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Scraper():
+    def __init__(self):
+        self.BASE_URL = "https://www.xe.com/currencyconverter/convert/?"
+        
+
+    def _getHtml(self, params: list):
+        amount, base, to = params
+        self.response = requests.get(url=f"{self.BASE_URL}Amount={amount}&From={base}&To={to}")
+                
+        return BeautifulSoup(self.response.content, 'html.parser')
+
+        
+    def getCurrency(self, params: list):
+        rawHtml = self._getHtml(params)
+        
+        currency = rawHtml.find('p', attrs={'class':'sc-262833c5-1 fOUdoA'})
+
+        return currency.text[:10]
---
+++ main.py
@@ -0,0 +1,3 @@
+from api.scraper import Scraper
+
+print(Scraper().getCurrency(["10","USD","EUR"]))
