d240eeb426d93db8840b8b84084c677a7a4a4275
focarica <a.vinicius0042@gmail.com>
2025-01-30 22:56:39 -0300

    changes to work on lambda function

--- py/api/scraper.py
+++ py/api/scraper.py
@@ -1,7 +1,6 @@
 import requests
+import json
 from bs4 import BeautifulSoup
-
-import string
 
 class Scraper():
     def __init__(self):
@@ -12,7 +11,7 @@ class Scraper():
     def _getHtml(self, params: list) -> BeautifulSoup:
         amount, base, to = params
         self.response = requests.get(url=f"{self.BASE_URL}Amount={amount}&From={base}&To={to}")
-                
+        
         return BeautifulSoup(self.response.content, 'html.parser')
 
         
@@ -22,3 +21,15 @@ class Scraper():
         currency = rawHtml.find('p', class_='sc-262833c5-1 fOUdoA')
 
         return currency.text[:9] # We only interest on the numbers
+
+def lambda_handler(event, context):
+    amount = event['amount']
+    base = event['base']
+    to = event['to']
+    
+    converted_amount = Scraper().getCurrency([amount, base, to])
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'convertedAmount': converted_amount})
+    }
