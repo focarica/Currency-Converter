e0ff3c6ef27ea52d16598a0dc75490c2dcadb46d
focarica <a.vinicius0042@gmail.com>
2025-02-05 11:24:52 -0300

    updated .gitignore

--- app/lib/fetcher.ts
+++ app/lib/fetcher.ts
@@ -7,8 +7,10 @@ const [baseCurrency, setBaseCurrency] = useState<string>("USD");
 const [targetCurrency, setTargetCurrency] = useState<string>("BRL");
 const [convertedAmount, setConvertedAmount] = useState<string | null>(null);
 
+const API_URL: string = process.env.NEXT_PUBLIC_API_URL!
+
 export const handleConvert = async () => {
-    const response = await fetch('https://f6gapsfeg2.execute-api.us-east-1.amazonaws.com/convert', {
+    const response = await fetch(API_URL, {
         method: 'POST',
         body: JSON.stringify({
         amount: amount,
